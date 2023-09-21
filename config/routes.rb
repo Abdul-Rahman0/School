95d5079d96993d6c64120b1d5f288eef8afa361c
abdurrahman <abdurhmankhan51@gmail.com>
2023-09-21 17:58:42 +0500

    added multiple functionality added devise creted diffrent roles and stylying

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  get 'teacher/index'
+  get 'portal/info'
+  devise_for :school_logins
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "home#index"
+
 end

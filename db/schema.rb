719d1095be545059ac195a376fe6045fc1a26b67
abdurrahman <abdurhmankhan51@gmail.com>
2023-10-10 10:58:53 +0500

    implemented cocoon

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_071921) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_124109) do
   create_table "class_names", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "class_names_teachers", id: false, force: :cascade do |t|
+    t.integer "class_name_id"
+    t.integer "teacher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["class_name_id"], name: "index_class_names_teachers_on_class_name_id"
+    t.index ["teacher_id"], name: "index_class_names_teachers_on_teacher_id"
   end
 
   create_table "classinfos", force: :cascade do |t|
@@ -53,19 +62,39 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_071921) do
   create_table "subjects", force: :cascade do |t|
     t.string "code"
     t.string "name"
-    t.integer "class_name_id"
+    t.integer "class_name_id", null: false
+    t.integer "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["class_name_id"], name: "index_subjects_on_class_name_id"
+    t.index ["teacher_id"], name: "index_subjects_on_teacher_id"
+  end
+
+  create_table "subjects_teachers", force: :cascade do |t|
+    t.integer "subject_id"
+    t.integer "teacher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_subjects_teachers_on_subject_id"
+    t.index ["teacher_id"], name: "index_subjects_teachers_on_teacher_id"
   end
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
     t.string "class_name"
-    t.integer "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["subject_id"], name: "index_teachers_on_subject_id"
+    t.integer "class_name_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_teachers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "subjects", "class_names"
+  add_foreign_key "subjects", "teachers"
+  add_foreign_key "teachers", "class_names"
 end
